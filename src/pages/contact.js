dddac01e9a521088af7ef8e2f712ac35356db529
daniel-korth <da.korth@icloud.com>
2021-04-09 20:49:31 +0200

    refactored contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -6,24 +6,8 @@ import SEO from "../components/seo"
 
 import Github from '../images/github.svg'
 import Email from '../images/eemail.svg'
-import Kaggle from '../images/kaggle-icon.svg'
+import Kaggle from '../images/kaggle.svg'
 import Discord from '../images/discord.svg'
-
-const Subtitle = styled.h3`
-  display: inline;
-  border-radius: 1em 0 1em 0;
-`
-
-// const Link = styled.a`
-//     margin-right: 25px;
-//     text-decoration: none;
-//     display: inline-block;
-//     position: relative;
-//     :hover {
-//         color:#de73ff;
-//         transform: scale(1.05);
-//     }
-// `
 
 const Icon = styled.a`
     color:#a5acb9;
@@ -31,10 +15,6 @@ const Icon = styled.a`
         color:#de73ff;
         transform: scale(1.05);
     };
-    margin: auto;
-    height: 200px;
-    width: 200px;
-    align: center;
 `
 
 const Content = styled.div`
@@ -43,10 +23,15 @@ const Content = styled.div`
   padding: 1.45rem 1.0875rem;
 `
 
-const IconDiv = styled.div`
+const IconList = styled.ul`
     margin: 0 auto;
-    width: 30vh;
-    height: 60vh;
+    list-style-type: none;
+`
+
+const ListItem = styled.li`
+    margin: auto;
+    height: 200px;
+    width: 200px;
     align: center;
 `
 
@@ -69,23 +54,29 @@ const IndexPage = () => (
                 <Link href='https://kaggle.com/danielkorth' target='_blank'>Kaggle</Link>
                 <Link href='https://discord.com/users/151430860149751808' target='_blank'>Discord</Link>
             </Contacts> */}
+            <IconList>
+                <ListItem>
+                    <Icon href='https://github.com/danielkorth' target='_blank'>
+                        {/* <svg width="64" height="64" aria-labelledby="svg-inline--fa-title-sPQsBQ9G1jID" data-prefix="fab" data-icon="github" class="svg-inline--fa fa-github fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><title id="svg-inline--fa-title-sPQsBQ9G1jID">Github</title><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg> */}
+                        <Github />
+                    </Icon>
+                </ListItem>
+                <ListItem>
+                    <Icon href='https://kaggle.com/danielkorth' target='_blank'>
+                        {/* <svg height="64" viewBox="0 0 32 32" width="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><clipPath id="a"><path d="m31.5 15.978h289v130.044h-289z"/></clipPath><path clip-path="url(#a)" d="m105.75 102.968c-.06.238-.298.357-.713.357h-7.937c-.477 0-.89-.208-1.248-.625l-13.106-16.672-3.655 3.477v12.93c0 .595-.298.892-.892.892h-6.152c-.595 0-.892-.297-.892-.892v-58.935c0-.593.297-.89.892-.89h6.153c.594 0 .892.298.892.89v36.288l15.692-15.87c.416-.415.832-.624 1.248-.624h8.204c.356 0 .593.15.713.445.12.357.09.624-.09.803l-16.585 16.046 17.297 21.488c.237.238.297.535.18.892" fill="#20beff" transform="matrix(.527027 0 0 .527027 -30.632288 -22.45559)"/></svg> */}
+                        <Kaggle />
+                    </Icon>
+                </ListItem>
+                <ListItem>
+                    <Icon href='mailto:da.korth@icloud.com' target='_blank'>
+                        <Email />
+                    </Icon>
+                </ListItem>
+                {/* <Icon href='https://discord.com/users/151430860149751808' target='_blank'>
+                    <Discord/>
+                </Icon> */}
+            </IconList>
         </Content>
-        <IconDiv>
-            <Icon href='https://github.com/danielkorth' target='_blank'>
-                {/* <svg width="64" height="64" aria-labelledby="svg-inline--fa-title-sPQsBQ9G1jID" data-prefix="fab" data-icon="github" class="svg-inline--fa fa-github fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"><title id="svg-inline--fa-title-sPQsBQ9G1jID">Github</title><path fill="currentColor" d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path></svg> */}
-                <Github />
-            </Icon>
-            <Icon href='https://kaggle.com/danielkorth' target='_blank'>
-                {/* <svg height="64" viewBox="0 0 32 32" width="64" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><clipPath id="a"><path d="m31.5 15.978h289v130.044h-289z"/></clipPath><path clip-path="url(#a)" d="m105.75 102.968c-.06.238-.298.357-.713.357h-7.937c-.477 0-.89-.208-1.248-.625l-13.106-16.672-3.655 3.477v12.93c0 .595-.298.892-.892.892h-6.152c-.595 0-.892-.297-.892-.892v-58.935c0-.593.297-.89.892-.89h6.153c.594 0 .892.298.892.89v36.288l15.692-15.87c.416-.415.832-.624 1.248-.624h8.204c.356 0 .593.15.713.445.12.357.09.624-.09.803l-16.585 16.046 17.297 21.488c.237.238.297.535.18.892" fill="#20beff" transform="matrix(.527027 0 0 .527027 -30.632288 -22.45559)"/></svg> */}
-                <Kaggle />
-            </Icon>
-            <Icon href='mailto:da.korth@icloud.com' target='_blank'>
-                <Email/>
-            </Icon>
-            {/* <Icon href='https://discord.com/users/151430860149751808' target='_blank'>
-                <Discord/>
-            </Icon> */}
-        </IconDiv>
     </Layout>
 )
 
