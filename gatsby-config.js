e8e8a0a5937b51034e474113d1d872cdbc3feeb9
daniel-korth <da.korth@icloud.com>
2021-04-20 21:35:23 +0200

    scroll plugins added

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-smoothscroll',
     // `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
