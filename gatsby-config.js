bdbfb5a31960539dfe59e65451ff20f285c19837
daniel-korth <da.korth@icloud.com>
2021-05-15 20:43:30 +0200

    new configs

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,11 +72,15 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/dk00.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // 'gatsby-plugin-offline',
+    {
+      resolve: `gatsby-plugin-remote-images`,
+      options: {
+        nodeType: 'RemoteImages',
+        imagePath: 'https://imgs.xkcd.com/comics/not_available.png',
+      },
+    },
   ],
 }
